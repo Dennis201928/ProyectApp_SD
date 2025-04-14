1de01b8f813b2dca8adfbe54d49bc2ded273964f
Dennis201928 <instagram.24@outlook.com>
2025-04-14 11:17:08 -0500

    corregido

--- src/main/java/com/proyect/app/Proyect/entidades/usuario/Usuario.java
+++ src/main/java/com/proyect/app/Proyect/entidades/usuario/Usuario.java
@@ -9,9 +9,9 @@ import lombok.Data;
 public class Usuario {
     @Id
     private long id;
-    private String nombre;
-    private String apellido;
-    private String cedula;
+    private String name;
+    private String lastname;
+    private String dni;
     private String email;
 
 }
